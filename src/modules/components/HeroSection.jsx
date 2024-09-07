007e7a9560eed26ed200076a133a2c0ff2e8083d
karan singh <you@example.com>
2024-09-07 17:50:44 +0530

    remove verify card

--- src/modules/components/HeroSection.jsx
+++ src/modules/components/HeroSection.jsx
@@ -4,12 +4,12 @@ const HeroSection = () => {
   return (
 
   <section className='px-4 lg:flex lg:py-8 lg:pt-12'>
-    <div className='veify_card md:hidden'>
+    {/* <div className='veify_card md:hidden'>
      <div className='bg-[#2087C2] rounded-lg min-h-28 min-w-28 mt-6 px-4 py-2 flex flex-col  justify-start items-start gap-6 '>
       <h2 className='text-white'>Account verifaction</h2>
        <button className='bg-white text-green-600 px-4 py-1 rounded-md font-medium'>Verify</button>
      </div>
-    </div>
+    </div> */}
 
 <div className="  w-full md:w-1/2 px-4 py-8  flex-col  justify-center items-center hidden lg:flex bg-red">
   <div className="max-w-lg mx-auto text-start">
